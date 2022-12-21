2b08e222d7bda949181f77e4c114ebf7a57a60e6
Andrew McLaughlin <3707913+mclaughlin@users.noreply.github.com>
2022-12-21 06:51:12 +0000

    Completely refactor code and fix recursion

--- README.md
+++ README.md
@@ -2,15 +2,95 @@
 
 Simple script to output a class' built-in subclasses and sub-subclasses.
 
-Example (from the CLI):
-
-```bash
-    ./psubclass.py parent_class_name
 ```
+Usage:
+    python3 psubclass.py <class_name>
+
+Example:
+    python3 psubclass.py Exception
 
 Example (from the REPL):
-
-```python
     >>> import psubclass
-    >>> psubclass.main('parent_class_name')
+    >>> psubclass.main()
+    Name of the class to find subclasses of: Exception
+
+Example output:
+    + ArgumentError
+    + ArgumentTypeError
+    + ArithmeticError
+    |-- + FloatingPointError
+    |-- + OverflowError
+    |-- + ZeroDivisionError
+    + AssertionError
+    + AttributeError
+    + BufferError
+    + ClassFoundException
+    + EOFError
+    + EndOfBlock
+    + Error
+    + ImportError
+    |-- + ModuleNotFoundError
+    |-- + ZipImportError
+    + LookupError
+    |-- + CodecRegistryError
+    |-- + IndexError
+    |-- + KeyError
+    + MemoryError
+    + NameError
+    |-- + UnboundLocalError
+    + OSError
+    |-- + BlockingIOError
+    |-- + ChildProcessError
+    |-- + ConnectionError
+    |-- |-- + BrokenPipeError
+    |-- |-- + ConnectionAbortedError
+    |-- |-- + ConnectionRefusedError
+    |-- |-- + ConnectionResetError
+    |-- + FileExistsError
+    |-- + FileNotFoundError
+    |-- + InterruptedError
+    |-- + IsADirectoryError
+    |-- + NotADirectoryError
+    |-- + PermissionError
+    |-- + ProcessLookupError
+    |-- + TimeoutError
+    |-- + UnsupportedOperation
+    |-- + itimer_error
+    + ReferenceError
+    + RuntimeError
+    |-- + NotImplementedError
+    |-- + RecursionError
+    |-- + _DeadlockError
+    + StopAsyncIteration
+    + StopIteration
+    + StopTokenizing
+    + SyntaxError
+    |-- + IndentationError
+    |-- |-- + TabError
+    + SystemError
+    |-- + CodecRegistryError
+    + TokenError
+    + TypeError
+    + ValueError
+    |-- + UnicodeError
+    |-- |-- + UnicodeDecodeError
+    |-- |-- + UnicodeEncodeError
+    |-- |-- + UnicodeTranslateError
+    |-- + UnsupportedOperation
+    + Verbose
+    + Warning
+    |-- + BytesWarning
+    |-- + DeprecationWarning
+    |-- + EncodingWarning
+    |-- + FutureWarning
+    |-- + ImportWarning
+    |-- + PendingDeprecationWarning
+    |-- + ResourceWarning
+    |-- + RuntimeWarning
+    |-- + SyntaxWarning
+    |-- + UnicodeWarning
+    |-- + UserWarning
+    + _OptionError
+    + error
+
 ```
--- psubclass.py
+++ psubclass.py
@@ -1,41 +1,85 @@
 #!/usr/bin/env python3
 
 """
-Simple script to output a class' sub (or child) classes
+Simple script to output (in a pretty format) a class' sub (or child) classes
+
+Usage:
+    python3 psubclass.py <class_name>
+
+Example:
+    python3 psubclass.py Exception
 """
 
 import argparse
 import sys
 
-__version__ = '0.0.1'
+def get_children(parent):
+    """
+    Returns a formatted list of the subclasses of the given parent class.
 
-class Subclasses:
+    Args:
+        parent (class): The parent class.
 
-    def children(self, parent, children=None):
-        """
-        Returns list of subclasses given a parent class
-        """
+    Returns:
+        List[class]: The subclasses of the parent class.
+    """
+    try:
+        return eval(parent).__subclasses__()
+    except NameError:
+        return []
 
-        if children is None:
-            children = []
-        try:
-            children = [ 
-                str(child)[8:-2:] 
-                for child in eval(parent).__subclasses__()
-            ]
-        except (AttributeError, NameError) as e:
-            pass
-        return children
+def sort_class(class_name):
+    return sorted(class_name, key=lambda x: x.__name__)
 
-def main(parent):
-    subclasses = Subclasses()
-    children = subclasses.children(parent)
-    for child in children:
-        print('-'*40+f'\n+ {child}')
-        sub_subclasses = Subclasses()
-        grandchildren = sub_subclasses.children(child)
-        for grandchild in grandchildren:
-            print(f'  - {grandchild}')
+def subclasses(parent, indent=""):
+    """
+    Recursively finds all subclasses of the given parent class.
+
+    Args:
+        parent (class): The parent class.
+    """
+    children = get_children(parent)
+    if children:
+        for child in sort_class(children):
+            name = child.__name__
+            print(f'{indent}+ {name}')
+            grandchildren = get_children(name)
+            if grandchildren:
+                subclasses(name, indent = indent + "|-- ")
+
+def get_input():
+    """
+    Prompts the user for input if the script is being run in the REPL, or
+    parses command-line arguments if the script is being run from the command line.
+
+    Returns:
+        str: The class name entered by the user.
+    """
+
+    input_help = 'Name of the class to find subclasses of: '
+    
+    if len(sys.argv) > 1:
+        #parse CLI args
+        # Parse command-line arguments
+        parser = argparse.ArgumentParser(description='Find subclasses of a given class: ')
+        parser.add_argument('class_name', type=str, help=input_help)
+        args = parser.parse_args()
+        return args.class_name
+    else:
+        #prompt user
+        return input(input_help)
+
+
+def main():
+    """
+    Get input and runs the script.
+    """
+
+    class_name = get_input()
+
+    # Find subclasses
+    subclasses(class_name)
 
 if __name__ == '__main__':
-    main(sys.argv[1:2][0])
+    main()
+
